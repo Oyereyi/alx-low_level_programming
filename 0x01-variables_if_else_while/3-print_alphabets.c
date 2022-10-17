56ba44ee64fc78c62089421d9d56568a8c09b54c
yusuf_zubair <zubairoyereyi@gmail.com>
2022-10-17 14:59:04 -0700

    upper case alphabet

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - to print alphabet in upper case
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		putchar(letter);
+
+	for (letter = 'A'; letter <= 'Z'; letter++)
+
+	putchar('\n');
+
+	return (0);
+}
