796c10c95164126185543a851da0149a360d280e
yusuf_zubair <zubairoyereyi@gmail.com>
2022-10-17 16:26:50 -0700

    last code

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - print all the combination of single units
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10 i++)
+	{
+		putchar((i % 10) + '0');
+		if (i == 9)
+			continue;
+		putchar(',');
+		putchar(' ');
+	}
+
+	putchar('\n');
+
+		return (0);
+}
