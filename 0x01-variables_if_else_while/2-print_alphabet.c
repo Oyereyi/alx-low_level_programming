0783378b3672f40ffe6896552bbeac359efc103f
yusuf_zubair <zubairoyereyi@gmail.com>
2022-10-16 21:29:22 -0700

    my

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_alphabet- Prints the print_alphabet
+ *
+ */
+void print_alphabet(void)
+{
+	char i;
+
+	printf("Alphabets from a-z:\n");
+
+	for (i = 'a'; i <= 'z'; i++)
+		putchar(i);
+	putchar("\n")
+}
